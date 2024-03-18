e3c431fa40e1b3cc91e3f4d57a8800281629a2ed
Stefan Petković <148398251+StefanPetk0vic@users.noreply.github.com>
2024-03-18 19:14:12 +0100

    Create ProcessRisingEdge.vhd

---
+++ SystemE/SystemI/ProcessRisingEdge.vhd
@@ -0,0 +1,25 @@
+-- Code your design here
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity ShiftRegister is
+	port (D : 	  in  std_logic_vector(3 downto 0);
+		Clock : in  std_logic;	
+		W : out std_logic_vector(3 downto 0);
+X : out std_logic_vector(3 downto 0);
+Y : out std_logic_vector(3 downto 0);
+Z : out std_logic_vector(3 downto 0));
+end entity;
+
+architecture ShiftRegister_arch of ShiftRegister is	
+begin
+	ShiftRegister_Proc : process (Clock)
+		begin
+			if (rising_edge(Clock)) then --falling edge za '0'
+				Z <= Y;				
+ 				Y <= X;
+				X <= W;
+				W <= D;
+			End if;
+		end process;
+end architecture;
